dc59aa7722179ba9e57800719821b51cc9d55a02
ravaattar <rava.naufal2014@gmail.com>
2022-04-03 19:08:24 +0700

    yuk dikit lagi

--- test2.py
+++ test2.py
@@ -15,6 +15,7 @@ initialPuzzle = {
     "cost" : 0,
     "lastMove" : -1
 }
+
 finalPuzzle = [
         [1,2,3,4],
         [5,6,7,8],
@@ -33,19 +34,57 @@ def riseNode(node):
         temp["gi"] = calculate_gi(temp["matrix"])
         temp["cost"] = temp["fi"] + temp["gi"]
         simpulHidup.append(temp)
-        # print(move)
-    return simpulHidup
+
+    return (simpulHidup)
 
 def sortSimpulHidup(simpulHidup):
-    temp = simpulHidup
-    
+    tempSimpulHidup = simpulHidup
+    for i in range(0, len(tempSimpulHidup)-1):
+        minIdx = i
+        for j in range (i+1, len(tempSimpulHidup)):
+            if(tempSimpulHidup[j]["cost"] < tempSimpulHidup[minIdx]["cost"]):
+                minIdx = j
+        tempVal = tempSimpulHidup[minIdx]
+        tempSimpulHidup[minIdx] = tempSimpulHidup[i]
+        tempSimpulHidup[i] = tempVal
 
-simpulE = initialPuzzle
-simpulH = riseNode(initialPuzzle)
-simpulFinal = [initialPuzzle]
-printMatrix(simpulE["matrix"])
+    return tempSimpulHidup
 
+simpulE = {}
+simpulFinal = []
+simpulH = [initialPuzzle]
+found = False
+printMatrix(initialPuzzle["matrix"])
+
+# for i in range(len(simpulH)):
+#     printMatrix(simpulH[i]["matrix"])
+#     print(simpulH[i])
+#     print()
+
+while(len(simpulH) != 0 and not found):
+    simpulE = simpulH.pop(0)
+    simpulFinal.append(simpulE)
+    if(simpulE["matrix"] == finalPuzzle):
+        simpulE["cost"] = simpulE["fi"]
+        found = True
+        break
+    risedNode = riseNode(simpulE)
+    for node in risedNode:
+        simpulH.append(node)
+    simpulH = sortSimpulHidup(simpulH)
+
+print("== PATH SIMPUL FINAL ==")
+for i in range(len(simpulFinal)):
+    print(simpulFinal[i])
+    print()
+    printMatrix(simpulFinal[i]["matrix"])
+
+print("== SIMPUL HIDUP ==")
 for i in range(len(simpulH)):
-    printMatrix(simpulH[i]["matrix"])
     print(simpulH[i])
     print()
+    printMatrix(simpulH[i]["matrix"])
+
+print("== STATUS FOUND ==")
+print(found)
+
